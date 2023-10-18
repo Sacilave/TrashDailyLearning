ea01978bf56491a6153d14b176c8aaf81c35b0a8
Sacilave Sifrua <sacilave@gmail.com>
2023-10-18 11:00:17 +0800

    C# day01

---
+++ C#/day01/RUSH/Program.cs
@@ -0,0 +1,62 @@
+﻿string friendName = "Steve";
+string spaceValue = "           abab     ababsjdfbjs  bafsbfjsadbkf     ";
+
+#region
+// 字符串
+
+// Console 类属于 System 文件中，但不使用 Using System; 直接调用 Console 时，调试器会自动调用
+Console.WriteLine($"ur friend name is : {friendName}, the length is {friendName.Length}");  // 字符串内插：使用 $" {变量名} " 格式进行字符串内插操作。.Length 可获取字符串长度，Length 是字符串类型
+
+Console.WriteLine($"{spaceValue}");
+// Trim 函数可以删除字符串前和后的空格，TrimStart 和 TrimEnd 分别删除前和后的空格
+string spaceValue1 = spaceValue.Trim();  // 在对字符串操作时需要重新赋值而不是直接修改
+
+//  Replace 函数可以替换字符串
+friendName = friendName.Replace("S", "A");
+
+// 分别为 转大写 和 转小写
+Console.WriteLine(friendName.ToUpper());
+Console.WriteLine(friendName.ToLower());
+
+// Contains 函数判断字符串是否存在，返回值为 true 或 false，但作为字符串输出时输出 True 和 False
+Console.WriteLine(spaceValue.Contains("abab"));
+
+// StartsWith 和 EndsWith 分别判断开头与结尾是否存在字符串，输出与 Contains 函数相同
+Console.WriteLine(friendName.StartsWith("St"));
+
+#endregion
+
+#region
+// 数字
+/*
+ * int
+ * float
+ * double
+ * decimal
+ * 
+ */
+// 获取类型的限度
+Console.WriteLine(double.MaxValue);
+Console.WriteLine(double.MinValue);
+Console.WriteLine(decimal.MaxValue);
+Console.WriteLine(decimal.MinValue);
+
+#endregion
+
+#region
+// 列表
+
+// 创建列表 使用 var 列表名 = new List<列表类型> {"","",""}
+var names = new List<string> { "Steve", "Lave" };
+// 列表 添加 和 删除 ：列表名.Add(值)  列表名.Remove(值)
+names.Add("Tom");  names.Remove("Steve");
+// 获取列表长度用 列表名.Count
+Console.WriteLine(names.Count);
+// 列表查找返回索引值使用 列表名.IndexOf(值)  返回值为此项的索引值，不存在则返回 -1
+foreach (string name in names)
+{
+    Console.WriteLine(names.IndexOf(name));
+}  // 如此可输出列表内所有元素的索引
+// 列表排序：使用 列表名.Sort()  如果为 String 类型列表 则按照字母顺序排序
+
+#endregion
---
+++ C#/day01/Tests/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Runtime;
+
+namespace test01
+{
+    class tests
+    {
+        static void output()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("\nWelcome");
+            for (int i = 0; i < 12; i++)
+            {
+                Console.Write("*");
+            }
+        }
+        static void calculateSquare()
+        {
+            Console.WriteLine("输入矩形的长");
+            float length = float.Parse(Console.ReadLine());
+            Console.WriteLine("输入矩形的宽");
+            float width = float.Parse(Console.ReadLine());
+            Console.WriteLine($"周长为：{Math.Round(2 * length * width, 2)}，面积为：{Math.Round(length * width, 2)}");
+        }
+        static void getMinL()
+        {
+            static int getMin(int a, int b)
+            {
+                if (a < b) return a;
+                else return b;
+            }
+            Console.WriteLine("输入第一个数");
+            int num01 = int.Parse(Console.ReadLine());
+            Console.WriteLine("输入第二个数");
+            int num02 = int.Parse(Console.ReadLine());
+            Console.WriteLine("输入第三个数");
+            int num03 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"最小数为：{getMin(getMin(num01, num02), num03)}");
+        }
+        static void NarcissisticNum()
+        {
+            Console.WriteLine("输入一个数: ");
+            double sum01 = 0;
+            int numIn = int.Parse( Console.ReadLine() );
+            int[] arr = new int[numIn.ToString().Length];
+            int tmp = numIn;
+            for(int i = numIn.ToString().Length; i != 0; i--)
+            {
+                arr[i-1] = numIn % 10;
+                numIn /= 10;
+            }
+            for (int i = 0; i < arr.Length; i++) sum01 += Math.Pow(arr[i], 3);
+            if ((sum01 - tmp) == 0) Console.WriteLine("是个水仙花数");
+            else Console.WriteLine("不是水仙花数");
+        }
+        static void unrepeat()
+        {
+            int[] arr = { 1, 2, 3, 4 };
+            int res = 0;
+            for (int i = 0; i<arr.Length; i++)
+            {
+                for (int j = 0; j<arr.Length; j++)
+                {
+                    for(int h = 0; h<arr.Length; h++)
+                    {
+                        Console.WriteLine($"{arr[i] * 100 + arr[j] * 10 + arr[h]}");
+                        res++;
+                    }
+                }
+            }
+            Console.WriteLine($"共可组成 {res} 个数字");
+        }
+        static void fish()
+        {
+            static int fishDivide(int num)
+            {
+                return num * 5 + 1;
+            }
+            Console.WriteLine(fishDivide(fishDivide(fishDivide(fishDivide(6)))));
+        }
+        static void issue07()
+        {
+            int numIn = int.Parse(Console.ReadLine());
+            bool flag = true;
+            int i = 2;
+            while(i < numIn-1){
+                if (numIn%i == 0)
+                {
+                    flag = false;
+                }
+                i++;
+            }
+            switch (flag)
+            {
+                case false: Console.WriteLine("不是素数"); break;
+                case true: Console.WriteLine("是素数"); break;
+            }
+        }
+        static void backNum()
+        {
+            Console.WriteLine("输入一个数判断是否为回文数");
+            int numIn = int.Parse(Console.ReadLine()); int tmp = numIn;
+            int[] arr = new int[numIn.ToString().Length];
+            bool flag = true;
+            for (int i = numIn.ToString().Length; i != 0; i--)
+            {
+                arr[i-1] = tmp%10;
+                tmp /= 10;
+            }
+            for (int i = 0; i < numIn.ToString().Length / 2 + 1; i++)
+            {
+                if (arr[i] != arr[numIn.ToString().Length-i-1])
+                {
+                    flag = false;
+                }
+            }
+            switch (flag)
+            {
+                case false: Console.WriteLine("不是回文数"); break;
+                case true : Console.WriteLine("是回文数"); break;
+            }
+        }
+        static void test()
+        {
+            int[] arr = new int[6];
+
+            foreach(int item in arr)
+            {
+                Console.WriteLine(arr.Length);
+            }
+        }
+        static void Main()
+        {
+            
+        }
+    }
+}
