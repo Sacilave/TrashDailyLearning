54b54625202d51a9d1dc2e59e7dcf4d32efaadb2
Sacilave <sacilave@gmail.com>
2023-11-21 23:38:02 +0800

    Unity C# notes

--- Unity/Notes/C#/PlayerControllerAnimated.cs
+++ Unity/Notes/C#/PlayerControllerAnimated.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerControllerAnimated : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator;
+    public int Speed = 3;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.Play("pickup");  // 播放名为 pickup 的 State
     }
 
     // Update is called once per frame
@@ -16,5 +18,26 @@ public class PlayerControllerAnimated : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 dir = new Vector3(horizontal, 0, vertical);  // 声明方向向量
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("IsRun", true);  // 设置名为 IsRun 的 Bool 为 true
+            Speed = 6;
+        }
+        else
+        {
+            animator.SetBool("IsRun", false);
+            Speed = 3;
+        }
+        if (dir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(dir);  // 让角色面向向量，方向与方向向量相同 (四元数类型)
+            animator.SetBool("IsWalk", true);
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);  // 向前移动 2 米
+        }
+        else animator.SetBool("IsWalk", false);
+        if (Input.GetKeyDown(KeyCode.F)) animator.SetTrigger("ifPickUp");  // 设置名为 ifPickUp 的 Trigger 为激活状态
+
+        Debug.Log(animator.GetFloat("TestCurve"));  // 获取名为 TestCurve 的参数值，同时也有 GetBool() 等等方法用于获取
+        
     }
 }
